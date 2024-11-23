40390180e9013d924feef2d71fab0e76bb4d1a9e
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2024-11-23 15:32:32 +0100

    generate post con php

--- index.php
+++ index.php
@@ -5,4 +5,22 @@ $autor='SantigoCorrea';
 
 $userName='@boss';
 
+$tweets= [
+    [
+        "texto"=> $texto,
+        "autor"=> $autor,
+        "userName" => $userName,
+    ],
+    [
+        "texto"=> "lorep isump",
+        "autor"=> "inteligencia artificial",
+        "userName" => "chap gpt",
+    ],
+    [
+        "texto"=> "En un lugar de la mancha",
+        "autor"=> "Cervantes",
+        "userName" => "MiguelC",
+    ],
+];
+
 require_once 'index.html';
